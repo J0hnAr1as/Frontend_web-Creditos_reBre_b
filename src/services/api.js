3347a36879cdda3185080a4bb137ca9b3496def4
J0hnAr1as <johnarias@tutamil.com>
2026-04-19 21:08:27 -0500

    se corrige api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://backend-creditos-re-bre-b.vercel.app/api"
+  baseURL: "https://backend-creditos-re-bre-b.vercel.app"
 })
 
 api.interceptors.request.use((config) => {
